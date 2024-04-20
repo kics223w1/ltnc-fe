21c91a8cca5c8bb08ae7837d66cc67e61e461a29
Cao Viết Huy <hieppro138@gmail.com>
2024-04-20 23:39:37 +0700

    refactor: logic management

--- src/renderer/components/staff/Management.tsx
+++ src/renderer/components/staff/Management.tsx
@@ -6,18 +6,20 @@ import { useToast } from '~/components/ui/use-toast';
 import { ToastAction } from '../../../~/components/ui/toast';
 import { Dialog, DialogTrigger } from '../../../~/components/ui/dialog';
 import DialogExaminationContent from '../dialog/ExaminationContent';
+import { MANAGEMENT_SERVICE } from '../../../main/models/constants';
 
 export default function Management() {
-  const [selectedExamination, setSelectedExamination] = useState<
-    Examination | undefined
-  >(undefined);
+  const [selectedExaminations, setSelectedExaminations] = useState<
+    Examination[]
+  >([]);
+  const [examinations, setExaminations] = useState<Examination[]>([]);
 
   const { toast } = useToast();
 
   useEffect(() => {}, []);
 
   const handleOnClickEdit = () => {
-    if (!selectedExamination) {
+    if (selectedExaminations.length === 0) {
       toast({
         title: 'Chưa có cuộc khám được chọn',
         description: 'Vui lòng chọn một cuộc khám',
@@ -26,21 +28,39 @@ export default function Management() {
       return;
     }
 
-    console.log(selectedExamination);
+    console.log(selectedExaminations);
+  };
+
+  const handleLoadExaminations = async () => {
+    const values = await window.electron.ipcRenderer.invoke(
+      MANAGEMENT_SERVICE.GET_EXAMINATIONS,
+      {}
+    );
+    setExaminations(values);
   };
 
   return (
-    <div className="flex flex-col w-full h-full px-12 pt-10">
-      {/* <ExaminationTable setSelectedExamination={setSelectedExamination} /> */}
+    <div className="flex flex-col w-full h-full px-12 pt-5">
+      <ExaminationTable
+        examinations={examinations}
+        handleLoadExaminations={handleLoadExaminations}
+        setSelectedExaminations={setSelectedExaminations}
+      />
 
       <div className="flex items-center justify-end gap-2 w-full mt-5">
         <Dialog>
-          <DialogTrigger asChild disabled={selectedExamination === undefined}>
+          <DialogTrigger asChild disabled={selectedExaminations.length === 0}>
             <Button onClick={handleOnClickEdit} variant={'default'} size={'lg'}>
               Chỉnh sửa
             </Button>
           </DialogTrigger>
-          <DialogExaminationContent examination={selectedExamination} />
+          <DialogExaminationContent
+            examination={
+              selectedExaminations.length > 0
+                ? selectedExaminations[selectedExaminations.length - 1]
+                : undefined
+            }
+          />
         </Dialog>
       </div>
     </div>
