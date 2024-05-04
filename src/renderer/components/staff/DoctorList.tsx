5efcd7cc9e5af5815ac96816387a88b8d9de5d84
Huy Cao <77295189+kics223w1@users.noreply.github.com>
2024-05-04 19:18:06 +0700

    refactor: init logic (#23)

--- src/renderer/components/staff/DoctorList.tsx
+++ src/renderer/components/staff/DoctorList.tsx
@@ -1,28 +1,17 @@
 import { useState } from 'react';
-import { Button } from '/~/components/ui/button';
 import DoctorTable from '../table/DoctorTable';
 import Doctor from '/main/models/doctor';
-import { useToast } from '/~/components/ui/use-toast';
 
 const DoctorList = () => {
   const [selectedDoctors, setSelectedDoctors] = useState<Doctor[]>([]);
-  const { toast } = useToast();
-
-  const handleSchedule = () => {
-    if (selectedDoctors.length === 0) {
-      toast({
-        variant: 'destructive',
-        title: 'Chưa chọn bác sĩ',
-        description: 'Vui lòng chọn ít nhất một bác sĩ để xem lịch khám',
-      });
-      return;
-    }
-  };
 
   return (
-    <div className="flex flex-col pt-10 px-12 w-full h-full">
+    <div className="flex flex-col pt-10 px-12 gap-5 w-full h-full">
       <div className="relative">
-        <DoctorTable setSelectedDoctors={setSelectedDoctors} />
+        <DoctorTable
+          isAllowSelection={false}
+          setSelectedDoctors={setSelectedDoctors}
+        />
       </div>
     </div>
   );
